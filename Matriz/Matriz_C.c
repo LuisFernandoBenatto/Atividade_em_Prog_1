ec0d88652309fdd00247213e7814016924135662
Luis Fernando Benatto <49990149+LuisFernandoBenatto@users.noreply.github.com>
2020-07-11 22:54:41 -0300

    Update Matriz_C.c

--- Matriz/Matriz_C.c
+++ Matriz/Matriz_C.c
@@ -1,49 +1,31 @@
 #include <stdio.h>
-#include <math.h>
 #include <stdlib.h>
-#include <string.h>
+#define LINHA 5
+#define COLUNA 4
 
+float matriz[LINHA][COLUNA] = {0};
 
-void print_matriz(int matriz[5][6]);
+void imprimir_matriz();
 
-int main(){
-    int i,j;
-    int soma = 0;
-    int matriz_C[5][6] = {{1,2,3,4,5},
-                          {2,4,6,8,10},
-                          {20,10,5,3,1},
-                          {3,6,9,12,15}};
-                          // J = COLUNAS;
-                          // I = LINHAS;
-    // começando nas colunas e depois as linhas;
-    for(j = 0; j < 6; j++){
-        for(i = 0, soma = 0; i < 5; i++){
-           matriz_C[5 - 1][j] += soma;
-           soma = matriz_C[i][j];
-        }
+int main() {
+  int i, j;
+  for(i=0; i < LINHA; i++) {
+    for(j=0; j < COLUNA; j++) {
+      printf("Preencha matriz[%d][%d]: ", i, j);
+      scanf("%f", &matriz[i][j]);
+      imprimir_matriz();
     }
-    // começando nas linhas e depois as colunas;
-    for(i = 0; i < 5; i++){
-        for(j = 0, soma =0; j < 6; j++){
-            matriz_C[i][6 - 1] += soma;
-            soma = matriz_C[i][j];
-        }
-    }
-
-    print_matriz(matriz_C);
-
-    system("pause");
-    return 0;
+  }
+  return 0;
 }
-void print_matriz(int matriz[5][6]){
 
-    int linha;
-    int coluna;
-
-    for(linha = 0; linha < 5; linha++){
-        for(coluna = 0; coluna < 6; coluna++){
-            printf("  %d  ", matriz[linha][coluna]);
-        }
-        printf("\n");
+void imprimir_matriz() {
+  int i, j;
+  system("cls");
+  for(i=0; i < LINHA; i++) {
+    for(j=0; j < COLUNA; j++) {
+      printf("%.2f  ", matriz[i][j]);
     }
+    printf("\n");
+  }
 }
